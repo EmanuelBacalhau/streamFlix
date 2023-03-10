3b960793cf30e6c2e9666ba74d69b421a6621c8b
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2023-03-10 04:09:03 -0300

    AdminJS
    Configuring the adminJS

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import express from "express";
+import { adminJs, adminJSRouter } from "./adminjs";
 import { sequelize } from "./database";
 
 const app = express();
+//      Caminho                     Rotas
+app.use(adminJs.options.rootPath, adminJSRouter);
+// Falar ao express que essa pasta é estática
+app.use(express.static("public"));
 
 const PORT = process.env.PORT || 3000;
 
